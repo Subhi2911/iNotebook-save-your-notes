a886637f8d08d1a885499ad9072a7a5a23cc0e79
Subhi2911 <msubhasmita29@gmail.com>
2025-06-27 15:31:07 +0530

    Making the front end

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import './App.css';
+import About from './components/About';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import { 
+  BrowserRouter as Router,
+  Routes,
+  Route
+ } from 'react-router-dom';
+import NoteState from './context/notes/NoteState';
+
+
+function App() {
+  return (
+    <>
+      <NoteState>
+        <Router>
+          <Navbar/>
+          <div className="container">
+            <Routes>
+              <Route path= '/about' element={<About/>}/>
+              <Route path='/' element={<Home/>}/>
+            </Routes>
+          </div>
+          
+        </Router>
+      </NoteState>
+    </>
+
+  );
+}
+
+export default App;
