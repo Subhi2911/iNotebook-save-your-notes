71fe94b27dda3e21d46a6f70b989f1ace1fcc3a2
Subhi2911 <msubhasmita29@gmail.com>
2025-07-06 15:55:30 +0530

    ui changes

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,5 +1,6 @@
 // components/Avatar.jsx
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 
 export default function Avatar({ name = "", size = 50, width= 11,
     height= 11 , theme = "light" }) {
@@ -11,13 +12,16 @@ export default function Avatar({ name = "", size = 50, width= 11,
     .join('')
     .toUpperCase();
 
+  const location=useLocation();
   const background = theme === 'light' ? '#0B1D51' : '#F1E7E7';
-  const color= theme==='light'?'#F1E7E7':'#0B1D51'
+  const color= theme==='light'?'#F1E7E7':'#0B1D51';
+  const bordercolor= (location.pathname==='/settings'||location.pathname==='/profile')?'#FFB433':'';
 
   const avatarStyle = {
     width:`${width}rem`,
     height:`${height}rem`, 
     borderRadius:'50%',
+    borderColor: bordercolor,
     backgroundColor: background,
     color: color,
     display: 'flex',
